b5944921defe911f142ff9d98d5a2d1e29e06195
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-05-16 19:51:11 +0530

    update

---
+++ a12/index.js
@@ -0,0 +1,8 @@
+const products=[
+    {id:1, name:"product 1",price:34},
+    {id:2, name:"product 2",price:45},
+    {id:3, name:"product 3",price:67},
+];
+products.forEach((x)=>{
+    console.log(x.name);
+});
