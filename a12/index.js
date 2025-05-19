7409121266ef9372dbeee942a9abe0f3caaf4e96
akshara-thotapali <thotapalliakshara2@gmail.com>
2025-05-19 19:57:21 +0530

    update

--- a12/index.js
+++ a12/index.js
@@ -1,8 +1,7 @@
-const products=[
-    {id:1, name:"product 1",price:34},
-    {id:2, name:"product 2",price:45},
-    {id:3, name:"product 3",price:67},
+const products = [
+  { id: 1, name: "Product 1", price: 34 },
+  { id: 1, name: "Product 2", price: 56 },
+  { id: 1, name: "Product 3", price: 40 },
 ];
-products.forEach((x)=>{
-    console.log(x.name);
-});
+
+products.forEach((product) => console.log(product.name));
--- a13/index.js
+++ a13/index.js
@@ -1,8 +1,14 @@
 const products=[
-    {id:1, name:"product 1",price:23},
-    {id:2, name:"product 2",price:45},
-    {id:3, name:"product 3",price:87},
-];
-products.forEach((x)=>
-    
-)
+    {id:1, name:"Product1", price:34},
+    {id:2, name:"Product2", price:56},
+    {id:3, name:"Product3", price:43},
+]
+
+const updatedProducts = products.map(product => ({
+    id: product.id,
+    name: product.name,
+    price: product.price + 5,
+}));
+updatedProducts.map(product => {
+  console.log(product.name, product.price);
+});
